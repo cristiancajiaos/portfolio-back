72bccc2ba018994d518eea3906c4652563904fe4
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-09-10 16:10:27 -0300

    Añadida app de Netlify para habilitar CORS

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     })
   )
   app.enableCors({
-    origin: ['http://localhost:4200']
+    origin: ['http://localhost:4200', 'https://portfolio-front-ccs.netlify.app']
   })
   await app.listen(process.env.PORT ?? 3000);
 }
